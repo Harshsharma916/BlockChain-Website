9f27083fe52e73daefdb42ac378f4843d811d2b7
Harshsharma916 <harsh.sharma@flamapp.com>
2022-04-03 00:54:33 +0530

    minor changes

--- src/Containers/HomePage/Components/Animate.js
+++ src/Containers/HomePage/Components/Animate.js
@@ -93,7 +93,7 @@ export function horizontalScroll() {
   function windowScroll(e) {
     var windowPosition = window.pageYOffset;
     window.scrollTo({
-      top: windowPosition + e.deltaY * 5,
+      top: windowPosition + e.deltaY * 3,
       behavior: "smooth",
     });
   }
